61b7410dba0418b67acc8f12e7aad4042e64d64b
Alexey Matveev <leshka99514@gmail.com>
2020-10-01 20:47:27 +0300

    добавил верстку по странице Main

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
-const Main = (props: any) => (
-    <div className="main__page">
-        <h3>Это главная страница</h3>
+const Main = () => (
+    <div className="main__page page">
+        <div className="page__modal modal">
+            <div className="modal__header">
+                <div className="inner">
+                    <span>Меню выбора</span>    
+                </div>      
+            </div>
+
+            <div className="modal__content">
+                <div className="inner">
+                    <span>Выберите: само приложение или почитать о приложении?</span>
+                </div>
+            </div>
+
+            <div className="modal__footer">
+                <div className="inner">
+                    <Link to="/about" className="link">О приложении</Link>
+                    <Link to="/app" className="link">В приложение</Link>
+                </div>
+            </div>
+        </div>
     </div>
 )
 
